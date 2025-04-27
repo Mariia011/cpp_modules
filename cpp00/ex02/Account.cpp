af22c7981b019c6abb8affc3fe9e3f5773af2581
Mariia Khachaturova <marikhac@c2r12s2.42yerevan.am>
2025-04-27 21:08:21 +0400

    a

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -1,2 +1,33 @@
 #include "Account.hpp"
 
+
+int	Account::getNbAccounts( void )
+{
+    
+}
+
+int	Account::getTotalAmount( void )
+{
+
+}
+
+
+int	Account::getNbDeposits( void )
+{
+
+}
+
+int	Account::getNbWithdrawals( void )
+{
+
+}
+
+void	Account::displayAccountsInfos( void )
+{
+
+}
+
+Account::Account( int initial_deposit )
+{
+
+}
